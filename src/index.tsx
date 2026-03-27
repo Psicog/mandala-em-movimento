1084b1ea8b7508eeeb5367949af3d0cbb19895ea
Mandala Dev <dev@mandala.com>
2026-03-27 19:43:59 +0000

    Atualiza ID do Meta Pixel para 1476098240620288

--- src/index.tsx
+++ src/index.tsx
@@ -24,11 +24,11 @@ app.get('/', (c) => {
   t.src=v;s=b.getElementsByTagName(e)[0];
   s.parentNode.insertBefore(t,s)}(window, document,'script',
   'https://connect.facebook.net/en_US/fbevents.js');
-  fbq('init', '2182098615887252');
+  fbq('init', '1476098240620288');
   fbq('track', 'PageView');
   </script>
   <noscript><img height="1" width="1" style="display:none"
-  src="https://www.facebook.com/tr?id=2182098615887252&ev=PageView&noscript=1"
+  src="https://www.facebook.com/tr?id=1476098240620288&ev=PageView&noscript=1"
   /></noscript>
   <!-- End Meta Pixel Code -->
   <link rel="preconnect" href="https://fonts.googleapis.com" />
